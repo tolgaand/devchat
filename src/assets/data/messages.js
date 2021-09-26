cf48161685947a61631da1fc3ff3756cd72f70eb
Tolga <tolgaand@yandex.com>
2021-09-27 00:57:27 +0300

    Create the messages data.

---
+++ src/assets/data/messages.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    author: {
+      id: 1,
+      name: 'Buğrahan',
+      lastname: 'Öztürk',
+      username: 'thebozturk',
+    },
+    content: 'My first nuxt project! amazing!!',
+  },
+  {
+    author: {
+      id: 0,
+      name: 'Tolga',
+      lastname: 'Çağlayan',
+      username: 'tolgaand',
+      avatarUrl:
+        'https://media-exp1.licdn.com/dms/image/C4D03AQGAySSfaE56MA/profile-displayphoto-shrink_200_200/0/1618134844892?e=1638403200&v=beta&t=FPwWn0moNGEHa4PKHRRASEYqsy2oPH4fZwqC2mIuUkU',
+    },
+    content: "I'm developing the devchata project!",
+  },
+]
