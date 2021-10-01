89415aa43c5e5c9adde6b4dd78e1acaf84dfb32b
Tolga <tolgaand@yandex.com>
2021-10-01 22:09:38 +0300

    Create the `LoadingBar` component

---
+++ src/components/LoadingBar.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  methods: {
+    start() {
+      this.loading = this.$vs.loading({
+        type: 'waves',
+      })
+    },
+    finish() {
+      if (this.loading) this.loading.close()
+    },
+  },
+}
+</script>
+
+<template lang="pug">
+.loading  
+</template>
+
+<style lang="scss" scoped></style>
